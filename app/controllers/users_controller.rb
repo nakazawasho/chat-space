0c435982e2bdd0f21e085255bc0598192f2c8f7d
nakazawasho <syodobsyo@gmail.com>
2017-08-08 01:14:37 +0900

    editメソッドのビューファイルを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
 
   def update
     User.update(update_params)
+    redirect_to root_path
   end
 
   private
   def update_params
-    params.require(:user).permit(:name, email:)
+    params.require(:user).permit(:name, :email)
   end
 end
