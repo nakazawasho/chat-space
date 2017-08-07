d8bad76ecf23aa84fabb27b99be66fe83668f005
nakazawasho <syodobsyo@gmail.com>
2017-08-08 00:52:47 +0900

    editに関するビューやアクションを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,2 @@
 class UsersController < ApplicationController
-  def edit
-  end
-
-  def update
-    current_user.update(update_params)
-    redirect_to "/"
-  end
-
-  private
-    def update_params
-      params.require(:user).permit(:name, :email)
-    end
 end
