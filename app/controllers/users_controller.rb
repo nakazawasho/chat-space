3ced47b9f8ac8196eb951c1abeb52d150e558ba7
nakazawasho <syodobsyo@gmail.com>
2017-08-08 01:11:33 +0900

    users_controllerにeditメソッドとupdateメソッドを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def edit
+  end
+
+  def update
+    User.update(update_params)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:name, email:)
+  end
 end
