4374073adba9b2b1a09cf5f3db6ea5acb6d0c74f
nakazawasho <syodobsyo@gmail.com>
2017-08-09 01:46:01 +0900

    修正＋マイグレーションファイルの編集

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,19 +1,2 @@
 class GroupsController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-
-  def show
-  end
-
-  def destroy
-  end
-
-  def edit
-  end
-
-  def  update
-  end
 end
