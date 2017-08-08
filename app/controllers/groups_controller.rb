28089df064393de229a507f2f0637189e5278827
nakazawasho <syodobsyo@gmail.com>
2017-08-09 01:27:03 +0900

    groupsコントローラーのアクションを定義

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,19 @@
 class GroupsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def  update
+  end
 end
