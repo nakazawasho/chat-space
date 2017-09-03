19b331759a5ae58c58d743199891200d64d38b10
nakazawasho <syodobsyo@gmail.com>
2017-09-04 00:43:07 +0900

    アソシエーションの定義

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   #association
   has_many :members
   has_many :users, through: :members
+  has_many :messages
 
   #validation
   validates :name, :user_ids, presence: true
