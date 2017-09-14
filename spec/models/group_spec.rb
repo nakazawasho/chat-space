60deea8ffccac24daff40a0c340b37046b67e2cf
nakazawasho <syodobsyo@gmail.com>
2017-09-14 19:51:26 +0900

    グループモデルのテストに、グループ名を入れないとエラーが起きることを確かめるテストを追加

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -7,5 +7,11 @@ describe Group do
       group.valid?
       expect(group).to be_valid
     end
+
+    it "is invalid without a name" do
+      group = build(:group, name: "")
+      group.valid?
+      expect(group.errors[:name]).to include("を入力してください")
+    end
   end
 end
