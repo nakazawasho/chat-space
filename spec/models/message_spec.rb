51c864ccfce684dc2d0c75d198ff01f4279f7975
nakazawasho <syodobsyo@gmail.com>
2017-09-22 15:01:07 +0900

    指摘箇所の修正

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,36 +1,42 @@
 require 'rails_helper'
 
 describe '#create' do
-  it 'is valid with a content, image, group_id, user_id, created_at, updated_at' do
+  #messageモデルで有効なファクトリを持つこと
+  it 'has a valid message_factory' do
     message = build(:message)
     expect(message).to be_valid
   end
 
-  it 'is valid with a content, group_id, user_id, created_at, updated_at' do
-    message = build(:message, image: "")
+  #imageに値が入っていなくても、contentに値が入って入れば有効である
+  it 'is valid with a content' do
+    message = build(:message, image: nil)
     expect(message).to be_valid
   end
 
-  it 'is valid with an image, group_id, user_id, created_at, updated_at' do
-    message = build(:message, content: "")
+  #contentに値が入っていなくても、imageに値が入って入れば有効である
+  it 'is valid with an image' do
+    message = build(:message, content: nil)
     expect(message).to be_valid
   end
 
+  #contentとimageに値が入っていなければ、有効でない。
   it 'is invalid without a content, image' do
-    message = build(:message, content: "", image: "")
+    message = build(:message, content: nil, image: nil)
     message.valid?
     expect(message.errors[:content]).to include("を入力してください。")
     expect(message.errors[:image]).to include("を入力してください。")
   end
 
+  #group_idに値が入っていなければ、有効でない。
   it 'is invalid without a group_id' do
-    message = build(:message, group_id: "")
+    message = build(:message, group_id: nil)
     message.valid?
     expect(message.errors[:group]).to include("を入力してください")
   end
 
+  #user_idに値が入っていなければ、有効でない。
   it 'is invalid without a user_id' do
-    message = build(:message, user_id: "")
+    message = build(:message, user_id: nil)
     message.valid?
     expect(message.errors[:user]).to include("を入力してください")
   end
