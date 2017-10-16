76ef429ffa9b5594962260f4dfa3eb757e23f756
nakazawasho <syodobsyo@gmail.com>
2017-10-16 23:49:02 +0900

    非同期の完成

---
+++ app/assets/javascripts/messages.js
@@ -0,0 +1,82 @@
+$(function(){
+  function buildHTML(message){
+    if (message.content != null && message.image_url != null){
+      var html = `<div class="message">
+                    <div class="message__name-date">
+                      <ul>
+                        <li class="list-name">
+                          ${message.user_name}
+                        </li>
+                        <li class="list-date">
+                          ${message.created_time}
+                        </li>
+                      </ul>
+                    </div>
+                  <div class="message_text">
+                    <p>
+                      ${message.content}
+                    </p>
+                      <img src="${message.image_url}">
+                    </div>
+                  </div>`
+      return html;
+    } else if(message.content != null && message.image_url == null){
+      var html = `<div class="message">
+                    <div class="message__name-date">
+                      <ul>
+                        <li class="list-name">
+                          ${message.user_name}
+                        </li>
+                        <li class="list-date">
+                          ${message.created_time}
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="message_text">
+                      <p>
+                        ${message.content}
+                      </p>
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="message__name-date">
+                      <ul>
+                        <li class="list-name">
+                          ${message.user_name}
+                        </li>
+                        <li class="list-date">
+                          ${message.created_time}
+                        </li>
+                      </ul>
+                    </div>
+                    <div class="message_text">
+                      <img src="${message.image_url}">
+                    </div>
+                  </div>`
+      return html;
+    }
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.message').append(html)
+      $('.textarea').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,4 @@
+json.image_url @message.image_url
+json.user_name @message.user.name
+json.created_time @message.created_time
+json.content @message.content
