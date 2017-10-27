1bbc652f067153de38c0861f1417c434b8743686
nakazawasho <syodobsyo@gmail.com>
2017-10-27 18:03:59 +0900

    js修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_filter :basic_authentication
-
-  def basic_authentication
-      authenticate_or_request_with_http_basic do |user, pass|
-      user == ENV['CHAT_SPACE'] && pass == ENV['CHAT_SPACE']
-  end
-
-  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
